8d5e21dcd770a2ad9be3d32436f0a9ea65528f83
rokrajher <rok.rajher8@gmail.com>
2021-02-22 16:49:54 +0100

    dodal funkcijo izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+
+    }
+
+    public static void izpis(int n, int k) {
+        System.out.println(n);
+        System.out.println(k);
+
     }
 }
