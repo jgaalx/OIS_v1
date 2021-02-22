538ef471b1cf5eac1d0e193d3e9b738a364b2403
Pinoxzy <pino.kokol@gmail.com>
2021-02-22 17:02:38 +0100

    dodal izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Gravitacija {
+    private static final double GRAVITACIJSKA_KONSTANTA = 6.674 * Math.pow(10, -11);
+    private static final double MASA_ZEMLJE = 5.972 * Math.pow(10, 24);
+    private static final double POLMER_ZEMLJE = 6.371 * Math.pow(10, 6);
+
+    
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nadVisina = sc.nextInt();
+
         System.out.println("OIS je zakon!");
+    }
+
+    public static double izracunPospeska(double nadVisina) {
+        double gravitacijskiPospesek = (GRAVITACIJSKA_KONSTANTA * MASA_ZEMLJE)/Math.pow((POLMER_ZEMLJE + nadVisina), 2);
+        return gravitacijskiPospesek;
     }
 }
